efccb8e3ea315b6821de4b7581ee002aed34b289
biancapower <bianca@biancapower.com>
2022-11-06 09:43:28 +1100

    Set up navigation & create initial screen

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import SearchScreen from "./src/screens/SearchScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
   },
-});
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Restaurant Search",
+    },
+  }
+);
+
+/*
+    The createAppContainer function creates a React component that wraps our navigator
+      and passes the navigation prop to our screens
+    This is necessary because otherwise, we wouldn't have any react component to export
+*/
+export default createAppContainer(navigator);
