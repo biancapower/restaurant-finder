c420a5f4811d7a9196cac0c222146fdf09a22f2a
biancapower <bianca@biancapower.com>
2022-11-06 16:56:02 +1100

    Add vertical scrolling & restrict to visible screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, ScrollView } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useRestaurants from "../hooks/useRestaurants";
 import RestaurantList from "../components/RestaurantList";
@@ -15,7 +15,7 @@ const SearchScreen = () => {
   };
 
   return (
-    <View>
+    <View style={{flex: 1}}>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)} // == onTermChange={setTerm}
@@ -26,10 +26,12 @@ const SearchScreen = () => {
       :
         null
       }
-      <RestaurantList title="Super Cheap" restaurants={filterRestaurantsByPrice("$")} />
-      <RestaurantList title="Kinda Cheap" restaurants={filterRestaurantsByPrice("$$")} />
-      <RestaurantList title="Bit Pricey" restaurants={filterRestaurantsByPrice("$$$")} />
-      <RestaurantList title="Pricey As" restaurants={filterRestaurantsByPrice("$$$$")} />
+      <ScrollView>
+        <RestaurantList title="Super Cheap" restaurants={filterRestaurantsByPrice("$")} />
+        <RestaurantList title="Kinda Cheap" restaurants={filterRestaurantsByPrice("$$")} />
+        <RestaurantList title="Bit Pricey" restaurants={filterRestaurantsByPrice("$$$")} />
+        <RestaurantList title="Pricey As" restaurants={filterRestaurantsByPrice("$$$$")} />
+      </ScrollView>
     </View>
   );
 }
