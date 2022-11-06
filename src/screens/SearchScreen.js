01a26d57d452c7e82545e42513b7d2341ef5c4d7
biancapower <bianca@biancapower.com>
2022-11-06 12:56:41 +1100

    Call yelp api on search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 5,
+        term, // == term: term
+        location: "Sydney"
+      }
+    })
+
+    setRestaurants(response.data.businesses);
+  };
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log(`Searching for ${term}...`)}
+        onTermChange={(newTerm) => setTerm(newTerm)} // == onTermChange={setTerm}
+        onTermSubmit={searchApi} // == onTermSubmit={() => searchApi()}
       />
-      <Text>{term}</Text>
+      <Text>We have found {restaurants.length} restaurants that match your search</Text>
     </View>
   );
 }
