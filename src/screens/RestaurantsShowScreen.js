7ef7fc9bee4097286250236f5da79f2890541bb3
biancapower <bianca@biancapower.com>
2022-11-06 17:50:07 +1100

    Render details to show page

--- src/screens/RestaurantsShowScreen.js
+++ src/screens/RestaurantsShowScreen.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, StyleSheet, View, FlatList, Image } from "react-native";
+import yelp from "../api/yelp";
 
-const ResultsShowScreen = ({ navigation }) => {
+const RestaurantShowScreen = ({ navigation }) => {
   const id = navigation.getParam("id");
-  console.log(id);
+  const [restaurant, setRestaurant] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const getRestaurant = async (id) => {
+    try {
+      const response = await yelp.get(`/${id}`);
+      setRestaurant(response.data);
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    getRestaurant(id);
+  }, []);
 
   return (
     <View>
-      <Text>Results Show Screen</Text>
+      <Text>{errorMessage ? errorMessage : null}</Text>
+      {restaurant ? (
+        <>
+          <Text>
+            {restaurant.name}
+          </Text>
+          <FlatList
+            data={restaurant.photos}
+            keyExtractor={(photo) => photo}
+            renderItem={({ item }) => {
+              return <Image style={styles.image} source={{ uri: item }} />;
+            }}
+          />
+        </>
+      )
+      :
+      <Text>Loading...</Text>}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300
+  }
+});
 
-export default ResultsShowScreen;
+export default RestaurantShowScreen;
