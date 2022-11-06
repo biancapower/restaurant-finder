89d75dbcb886dd659ae64c0cd5f3ccfa091a8918
biancapower <bianca@biancapower.com>
2022-11-06 14:58:07 +1100

    Use useEffect to render results on app startup

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
@@ -8,12 +8,12 @@ const SearchScreen = () => {
   const [restaurants, setRestaurants] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get("/search", {
         params: {
           limit: 5,
-          term, // == term: term
+          term: searchTerm,
           location: "Sydney"
         }
       })
@@ -25,12 +25,20 @@ const SearchScreen = () => {
     }
   };
 
+  // Call searchApi when component is first rendered
+  // The second argument is an array of dependencies
+  // If the array is empty, the callback will only be called once
+  useEffect(() => {
+    searchApi("keto");
+  }, []);
+
+
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)} // == onTermChange={setTerm}
-        onTermSubmit={searchApi} // == onTermSubmit={() => searchApi()}
+        onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ?
         <Text>{errorMessage}</Text>
