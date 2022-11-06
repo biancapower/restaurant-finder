77a664378ee2bb3e21072f6128d02c496c2f4a44
biancapower <bianca@biancapower.com>
2022-11-06 15:07:03 +1100

    Extract business logic into custom hook

---
+++ src/hooks/useRestaurants.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import yelp from "../api/yelp";
+
+export default () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 5,
+          term: searchTerm,
+          location: "Sydney"
+        }
+      })
+
+      setRestaurants(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Sorry, something went wrong");
+      setRestaurants([]);
+    }
+  };
+
+  // Call searchApi when component is first rendered
+  // The second argument is an array of dependencies
+  // If the array is empty, the callback will only be called once
+  useEffect(() => {
+    searchApi("keto");
+  }, []);
+
+  return [searchApi, restaurants, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,37 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useRestaurants from "../hooks/useRestaurants";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [restaurants, setRestaurants] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 5,
-          term: searchTerm,
-          location: "Sydney"
-        }
-      })
-
-      setRestaurants(response.data.businesses);
-    } catch (err) {
-      setErrorMessage("Sorry, something went wrong");
-      setRestaurants([]);
-    }
-  };
-
-  // Call searchApi when component is first rendered
-  // The second argument is an array of dependencies
-  // If the array is empty, the callback will only be called once
-  useEffect(() => {
-    searchApi("keto");
-  }, []);
-
+  const [searchApi, restaurants, errorMessage] = useRestaurants();
 
   return (
     <View>
