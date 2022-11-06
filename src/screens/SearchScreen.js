bd690d436d5ebbf4f32b287cf9cf0995f46c464e
biancapower <bianca@biancapower.com>
2022-11-06 11:13:22 +1100

    Add state to search screen and search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+
   return (
     <View>
-      <SearchBar />
-      <Text>SearchScreen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+      />
+      <Text>{term}</Text>
     </View>
   );
 }
